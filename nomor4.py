6d29be5d7eb9ffc0af26743204dfc399bcd8bbf6
carolik14 <117900960+carolik14@users.noreply.github.com>
2022-11-23 22:33:03 +0700

    Add files via upload

---
+++ nomor4.py
@@ -0,0 +1,6 @@
+k = int(input("Masukkan keceptan tempuh : "))
+w = int(input("Masukkan waktu yang diperlukan : "))
+j = (k*w)/10
+j2 = j*15000
+print("Teman Anda mengisi bensin sebanyak", j, "liter")
+print("Biaya yang dikeluarkan untuk mengisi bensin adalah Rp.", j2)
