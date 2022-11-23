7fd31bc07dbad74d3cc77585887a693e87fae920
carolik14 <117900960+carolik14@users.noreply.github.com>
2022-11-23 22:33:57 +0700

    Add files via upload

---
+++ carol.py
@@ -0,0 +1,10 @@
+print("nomor 1.a")
+j = int(input("masukkan jari-jari : "))
+phi = 3.14
+luas = phi*j**2
+print("hasil = ", luas)
+print("nomor 1.b")
+j = int(input("masukkan jari-jari : "))
+phi = 3.14
+luas = phi*j**2
+print("hasil = ", luas)
---
+++ lagi.py
@@ -0,0 +1,6 @@
+print("nomor 2.a")
+a = int(input("masukkan alas atap : "))
+t_a = int(input("masukkan tinggi atap : "))
+t_t = int(input("masukkan tinggi tembok :"))
+luas = ((1/2*a*t_a)+(t_t**2))/5
+print("rumah tersebut membutuhkan", luas, "sak semen")
