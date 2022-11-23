0ca4289cb444a28f44ae8416c43398ed9702b202
carolik14 <117900960+carolik14@users.noreply.github.com>
2022-11-23 22:31:01 +0700

    Add files via upload

---
+++ nomor3.py
@@ -0,0 +1,20 @@
+print("Nomor 3")
+nama = input("Masukkan nama anda : ")
+p = nama.split(" ")
+d = p[0]
+b = p[1]
+matkul = input("Masukkan mata kuliah : ")
+grup = input("Masukkan grup : ")
+print("hallo!", b, ",", d)
+print(d, "tergabung dalam kelas", matkul, "pada grup", grup)
+
+print("Nomor 3")
+nama = input("Masukkan nama anda : ")
+p = nama.split(" ")
+d = p[0]
+b = p[1]
+c = p[2]
+matkul = input("Masukkan mata kuliah : ")
+grup = input("Masukkan grup : ")
+print("hallo!", b, ",", d, c)
+print(d, "tergabung dalam kelas", matkul, "pada grup", grup)
